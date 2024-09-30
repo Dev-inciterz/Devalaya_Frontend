e475d683a5903e87196345b70f08faf87426d6ab
Mac <mac@Macs-MacBook-Pro.local>
2024-09-30 12:36:10 +0530

    regular push

--- src/Components/Home/ShaktiPeeths.jsx
+++ src/Components/Home/ShaktiPeeths.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./ShaktiPeeths.css";
 import { axiosWithoutToken } from "../../Api/AxiosInstance";
-import shaktipeeth from "../Assets/main shaktipeeth.jpg";
+// import shaktipeeth from "../Assets/main shaktipeeth.jpg";
 
 const ShaktiPeeths = () => {
   const [shaktiPeethas, setShaktiPeethas] = useState([]);
